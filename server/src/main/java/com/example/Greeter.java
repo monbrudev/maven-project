40c93219181917622c08257620977fc307facafa
ramon <monbru@gmail.com>
2019-11-22 12:47:59 +0100

    fix checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,8 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-
+/** {@inheritDoc} **/
   public String greet(final String someone) {
+    /**
+    * This is a Javadoc required comment
+    */	  
     return String.format("Hello, %s!", someone);
   }
 }
